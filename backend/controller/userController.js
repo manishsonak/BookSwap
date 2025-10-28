085124519e35f25bb35ff38b37dd4c4643e65365
manishsonak <manishsonak9@gmail.com>
2025-10-28 18:18:25 +0530

    change in cookies

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -19,7 +19,7 @@ export const registerUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -47,7 +47,7 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
